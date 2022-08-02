ec935ee3237ae8f331c42d6b4efcca6c8be4b47f
Wade <wbroem01@louisville.edu>
2022-08-02 18:27:39 -0500

    clustering analysis working, started plots

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MScThesis
 This is the repository for my MSc thesis project, under the mentorship of Dr. Benne Holwerda at the University of Louisville. The goal of this project is to apply machine learning to 30min TESS lightcurves in order to identify gravitational lenses. 
+I hope to make the project entirely reproducible using the code in the file msc_tools.py and some of the code in the Implement_ML notebook. The TESS data that was obtained from MAST is not stored in this repository since it is nearly 4GB in total. If you want to obtain the same data set, open a python environment and run:
+
+msc_tools.get_all_mast_data()
+
+in the directory that you want the data stored in.
--- msc_tools.py
+++ msc_tools.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import time
 
+#TODO: make the mast_data_path a parameter in the function call so that other people can use this on their machine
 mast_data_path = '/Users/waderoemer/Desktop/MScThesis/MAST_data'
 
 kids_data = pd.read_csv('KiDS_lenses.csv',skiprows=7)
@@ -35,3 +36,50 @@ def remove_zip_files():
     for zipfile in zipfiles:
         zipfile = zipfile.replace(r'&',r'\&')
         os.system(f'cd MAST_data\n rm {zipfile}')
+        
+
+def rebin_lc(data):
+    flux = []
+    i = 0
+    while i <= len(data['FLUX']):
+        flux_i = data['FLUX'][i][2][2] + data['FLUX'][i+1][2][2] + data['FLUX'][i+2][2][2]
+        flux.append(flux_i)
+        i += 3
+        # if len(data['FLUX']) is not of length 3n then we have to drop the last 1-2 values
+        if i+3 > len(data['FLUX']):
+            break
+    return flux
+
+
+def produce_the_matrix():
+    matrix = [] # this will be the array of LCs
+    n_bins = 968
+    
+    for file in os.listdir(mast_data_path):
+        try:
+            hdu = fits.open(mast_data_path + file)
+            data = hdu[1].data
+            flux = []
+            sector = hdu['PRIMARY'].header['SECTOR']
+            if sector >= 27:
+                flux = rebin_lc(data)               
+                for i in range(len(flux)):
+                    flux_i = flux[i]
+                    flux.append(flux_i)
+                flux = flux / np.max(flux) # normalize each LC
+                flux = flux[:n_bins] # trim to 24 days
+                matrix += [flux]
+                
+            else:         
+                for i in range(len(data['FLUX'])):
+                    flux_i = data['FLUX'][i][2][2] # [frame][row][col]
+                    flux.append(flux_i)
+                flux = flux / np.max(flux) # normalize each LC
+                flux = flux[:n_bins] # trim to 24 days
+                matrix += [flux]
+            
+        except OSError:
+            continue
+    
+    return matrix
+
